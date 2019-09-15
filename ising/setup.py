878fafc9ea668ed258ff3ff55df8a783f9e1eaba
Rajesh Singh <rajeshrinet@gmail.com>
2019-09-15 23:13:56 +0100

    exponent code

--- ising/setup.py
+++ ising/setup.py
@@ -7,51 +7,47 @@ Cython.Compiler.Options.annotate = True
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-def check_for_openmp():
-    ##Adapted from Goldbaum reply. See https://github.com/pynbody/pynbody/issues/124
-    # Create a temporary directory
+def checkOpenmpSupport():
+    """ Adapted from https://stackoverflow.com/questions/16549893/programatically-testing-for-openmp-support-from-a-python-setup-script
+    """ 
+    ompTest = \
+    r"""
+    #include <omp.h>
+    #include <stdio.h>
+    int main() {
+    #pragma omp parallel
+    printf("Thread %d, Total number of threads %d\n", omp_get_thread_num(), omp_get_num_threads());
+    }
+    """
     tmpdir = tempfile.mkdtemp()
     curdir = os.getcwd()
     os.chdir(tmpdir)
 
-    # Get compiler invocation
-    compiler = os.getenv('CC', 'cc')
-
-    # Attempt to compile a test script.
-    # See http://openmp.org/wp/openmp-compilers/
     filename = r'test.c'
-    file = open(filename,'w', 0)
-    file.write(
-        "#include <omp.h>\n"
-        "#include <stdio.h>\n"
-        "int main() {\n"
-        "#pragma omp parallel\n"
-        "printf(\"Hello from thread %d, nthreads %d\\n\", omp_get_thread_num(), omp_get_num_threads());\n"
-        "}")
+    with open(filename, 'w') as file:
+        file.write(ompTest)
     with open(os.devnull, 'w') as fnull:
-        exit_code = subprocess.call([compiler, '-fopenmp', filename],
-                                    stdout=fnull, stderr=fnull)
-    # Clean up
-    file.close()
-    os.chdir(curdir)
-    shutil.rmtree(tmpdir)
+        result = subprocess.call(['cc', '-fopenmp', filename],
+                                 stdout=fnull, stderr=fnull)
 
-    if exit_code == 0:
+    os.chdir(curdir)
+    shutil.rmtree(tmpdir) 
+    if result == 0:
         return True
     else:
         return False
 
-if check_for_openmp() == True:
-    omp_args = ['-fopenmp']
+if checkOpenmpSupport() == True:
+    ompArgs = ['-fopenmp']
 else:
-    omp_args = None
+    ompArgs = None 
 
 ext_modules=[
     Extension("ising",
               sources=["core/ising.pyx", "core/cIsing.c"],
               include_dirs=[numpy.get_include()],
-              extra_compile_args=omp_args,
-              extra_link_args=omp_args,
+              extra_compile_args=ompArgs,
+              extra_link_args=ompArgs,
               libraries=[]), # Unix-like specific
 ]
 
